a5cffd97054084f1c2f3aa215ed16e38403a2661
marian2js <marianomdq1@gmail.com>
2016-10-14 21:25:59 -0300

    fix: Map languages into tesseract

--- ai/utils/ocr_utils.py
+++ ai/utils/ocr_utils.py
@@ -5,9 +5,16 @@ import pyocr.builders
 ocr = pyocr.get_available_tools()[0]
 
 
-def read_text(image_data, lang='eng'):
+def read_text(image_data, lang='english'):
     return ocr.image_to_string(
         Image.fromarray(image_data),
-        lang=lang,
+        lang=_map_language(lang),
         builder=pyocr.builders.TextBuilder()
     )
+
+
+def _map_language(lang):
+    return {
+        'english': 'eng',
+        'spanish': 'spa',
+    }.get(lang, 'eng')
